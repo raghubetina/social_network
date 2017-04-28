5129d756de88f107d7ae6807fc727b47c1e9e18a
Raghu Betina <raghu@firstdraft.com>
2017-04-28 00:35:14 +0000

    Direct association between users and likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :dependent => :destroy
+
   has_many   :uploads,
              :class_name => "Photo",
              :dependent => :destroy
